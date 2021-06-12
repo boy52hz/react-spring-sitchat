8f8dbe10e3bdf4bfb62d704f5bde7270d369d8af
Rooted <modhanami@hotmail.co.th>
2021-06-12 20:43:35 +0700

    Add websocket base

---
+++ server/src/main/java/com/sitchat/server/websocket/ChatMessage.java
@@ -0,0 +1,38 @@
+package com.sitchat.server.websocket;
+
+public class ChatMessage {
+    private String from;
+    private String to;
+    private String content;
+
+    public ChatMessage(String from, String to, String content) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
