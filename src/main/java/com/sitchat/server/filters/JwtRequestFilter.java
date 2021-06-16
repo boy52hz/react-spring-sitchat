c8b9b76831f0ffd639ec9df49fc5fce70c2dfb14
Rooted <modhanami@hotmail.co.th>
2021-06-16 19:04:50 +0700

    Add response for invalid tokens

--- src/main/java/com/sitchat/server/filters/JwtRequestFilter.java
+++ src/main/java/com/sitchat/server/filters/JwtRequestFilter.java
@@ -1,9 +1,11 @@
 package com.sitchat.server.filters;
 
+import com.google.gson.Gson;
 import com.sitchat.server.services.imp.UserDetailsServiceImp;
 import com.sitchat.server.utils.JwtUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +19,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -33,21 +38,35 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtUtil.extractUsername(jwt);
-        }
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails authDetails = userDetailsServiceImp.loadUserByUsername(username);
-            if (jwtUtil.validateToken(jwt, authDetails)) {
-                UsernamePasswordAuthenticationToken userPassAuthToken = new UsernamePasswordAuthenticationToken(
-                        authDetails, null, authDetails.getAuthorities());
-                userPassAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(userPassAuthToken);
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                jwt = authorizationHeader.substring(7);
+                username = jwtUtil.extractUsername(jwt);
             }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails authDetails = userDetailsServiceImp.loadUserByUsername(username);
+                if (jwtUtil.validateToken(jwt, authDetails)) {
+                    UsernamePasswordAuthenticationToken userPassAuthToken = new UsernamePasswordAuthenticationToken(
+                            authDetails, null, authDetails.getAuthorities());
+                    userPassAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(userPassAuthToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (JwtException ex) {
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "Invalid token");
+            response.setStatus(401);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter out = response.getWriter();
+            out.print(new Gson().toJsonTree(map).getAsJsonObject());
+            out.flush();
         }
 
-        filterChain.doFilter(request, response);
+
     }
 }
